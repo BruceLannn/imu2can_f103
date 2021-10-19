1cdb21090e5ba3ce188f05fea1eb0d668d25065d
BruceLannn <1656257449@qq.com>
2021-10-19 19:31:50 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/DependInfo.cmake
@@ -0,0 +1,82 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/bruce/Documents/imu2can_f103/startup/startup_stm32f103x6.s" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/startup/startup_stm32f103x6.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../applications"
+  "../bsp/boards"
+  "../component/devices"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_can.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_can.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Src/main.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Src/main.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Src/stm32f1xx_hal_msp.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Src/stm32f1xx_it.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Src/stm32f1xx_it.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Src/syscalls.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Src/syscalls.c.obj"
+  "/home/bruce/Documents/imu2can_f103/Src/system_stm32f1xx.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/Src/system_stm32f1xx.c.obj"
+  "/home/bruce/Documents/imu2can_f103/applications/imu_interrupt.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/applications/imu_interrupt.c.obj"
+  "/home/bruce/Documents/imu2can_f103/applications/pid.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/applications/pid.c.obj"
+  "/home/bruce/Documents/imu2can_f103/bsp/boards/bsp_can.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/bsp/boards/bsp_can.c.obj"
+  "/home/bruce/Documents/imu2can_f103/bsp/boards/imu_pwm.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/bsp/boards/imu_pwm.c.obj"
+  "/home/bruce/Documents/imu2can_f103/component/devices/BMI088Middleware.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/component/devices/BMI088Middleware.c.obj"
+  "/home/bruce/Documents/imu2can_f103/component/devices/BMI088driver.c" "/home/bruce/Documents/imu2can_f103/cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/component/devices/BMI088driver.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F103x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../applications"
+  "../bsp/boards"
+  "../component/devices"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/imu2can_f103.elf.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_can.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_spi.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Src/main.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Src/stm32f1xx_it.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Src/syscalls.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/Src/system_stm32f1xx.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/applications/imu_interrupt.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/applications/pid.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/bsp/boards/bsp_can.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/bsp/boards/imu_pwm.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/component/devices/BMI088Middleware.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/component/devices/BMI088driver.c.obj"
+  "CMakeFiles/imu2can_f103.elf.dir/startup/startup_stm32f103x6.s.obj"
+  "imu2can_f103.elf"
+  "imu2can_f103.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/imu2can_f103.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
